00c94e502b30b2695794cd89c399f9a90a351391
njacques47 <nojacques94@gmail.com>
2022-01-20 21:15:01 -0500

    added health status updates to the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,16 +2,45 @@ var playerName = window.prompt("What is you robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-// you can also log multiple values at once like this
+// you can also log multiple values at once like this. when you console.log playerName that's when you 
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roberto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
+// this is an example of a function expression where a function is created by assigned it to a variable then later executed. 
 var fight = function() {
-    window.alert("Welcome to Robot Gladiators!")
+    // alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable 
+    enemyHealth = enemyHealth - playerAttack;
+
+     // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    };
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+     // check player's health
+     console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+     if (playerHealth <= 0) {
+         window.alert(playerName + " has died!");
+     }
+     else {
+         window.alert(playerName + " still has " + playerHealth + " health left.");
+     }
 };
 
-// this is where the fight function gets called [activated]
+// this is where the fight function gets called [executed]
 fight();
